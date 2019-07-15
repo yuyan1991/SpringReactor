e4bb78facf1bdd7f8f1a36d833414cbec442a602
yuyan <yuyan1991@gmail.com>
2019-07-15 15:45:03 +0800

    refactor as reactive web application

--- src/main/java/com/qrqs/spring/reactor/controller/HelloController.java
+++ src/main/java/com/qrqs/spring/reactor/controller/HelloController.java
@@ -1,12 +1,16 @@
 package com.qrqs.spring.reactor.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
+@Slf4j
 public class HelloController {
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello World!";
+    public Mono<String> hello() {
+        log.info("Enter into hello()");
+        return Mono.just("Hello World!");
     }
 }
