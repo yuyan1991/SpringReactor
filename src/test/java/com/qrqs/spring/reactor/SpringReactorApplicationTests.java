c4f93b3dd3fadafc1ea72121d9d5383e547e3aab
yuyan <yuyan1991@gmail.com>
2019-07-15 12:12:31 +0800

    initial commit of Spring Reactor demo

---
+++ src/test/java/com/qrqs/spring/reactor/SpringReactorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qrqs.spring.reactor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringReactorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
